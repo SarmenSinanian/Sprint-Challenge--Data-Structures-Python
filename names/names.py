7b61e3424f3260011c169d9fb2a9d04abb51e3a2
tooltipforyou <52585634+SarmenSinanian@users.noreply.github.com>
2020-02-28 10:51:22 -0800

    Add files via upload

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,11 +11,37 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Starter code has a time complexity of O(n^2)
+
+# duplicates = []
+# # for name_1 in names_1:
+# #     for name_2 in names_2:
+# #         if name_1 == name_2:
+# #             duplicates.append(name_1)
+
+
+# Time complexity of O(n log n)
+
+# Create root node
+binary_search_tree = BinarySearchTree(names_1[0])
+
+# Create empty duplicate array
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# Start for loop at 2nd item (item after root which was used to create the tree)
+for item in names_1[1:]:
+# Insert items iteratively into the binary search tree
+    binary_search_tree.insert(item)
+# Start for loop at first item because searching tree, not making tree
+for item in names_2:
+# Check if tree contains item
+    if binary_search_tree.contains(item):
+# Add item to duplicates array if item is within tree (aka duplicate item)
+        duplicates.append(item)
+
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
